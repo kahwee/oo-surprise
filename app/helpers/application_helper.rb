a9d01ac943386572475ad41ed3b1fc4d35838564
KahWee Teng <t@kw.sg>
2011-01-06 18:11:31 +0800

    Moved the overall design into appropriate helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def jqmobile_theme(segment)
+    case segment
+      when "data" then "c"
+      when "data-count" then "e"
+      when "data-divider" then "d"
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OoExperiment::Application.routes.draw do
   resources :servers
+  resources :tasks
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
