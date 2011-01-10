317a6dda30b63e0e0533cd969d9ed4ae9c6da0f0
KahWee Teng <t@kw.sg>
2011-01-10 13:09:59 +0800

    Nicer statuses.

--- app/models/server_task_status.rb
+++ app/models/server_task_status.rb
@@ -5,4 +5,10 @@ class ServerTaskStatus < ActiveRecord::Base
   
   belongs_to :server
   belongs_to :task
+  
+  STATUSES = [
+    ['Not applicable', 0], 
+    ['Pending', 1], 
+    ['Done', 9]
+  ]
 end
